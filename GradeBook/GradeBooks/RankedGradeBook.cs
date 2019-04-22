afa5a33bf8417f6a02610c82ae9dea4cda65037a
Patryk Kubacki <patryk.kubacki@outlook.com>
2019-04-22 12:15:12 +0200

    Override RankedGradeBook's GetLetterGrade 2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,7 +16,7 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if(Students.Count <= 5)
+            if(Students.Count < 5)
                 throw new InvalidOperationException();
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
@@ -24,11 +24,11 @@ namespace GradeBook.GradeBooks
 
             if (grades[threshold - 1] <= averageGrade)
                 return 'A';
-            else if (grades[threshold * 2 - 1] <= averageGrade)
+            if (grades[threshold * 2 - 1] <= averageGrade)
                 return 'B';
-            else if (grades[threshold * 3 - 1] <= averageGrade)
+            if (grades[threshold * 3 - 1] <= averageGrade)
                 return 'C';
-            else if (grades[threshold * 4 - 1] <= averageGrade)
+            if (grades[threshold * 4 - 1] <= averageGrade)
                 return 'D';
             return 'F';
         }
